1c5f1a29ff25a3ea56f524776191efb31429aed8
julio p sihite <julio.sihite@finaccel.co>
2021-04-11 13:06:32 +0700

    create initial tailwind

--- day-5/fresh-market/src/pages/Login.js
+++ day-5/fresh-market/src/pages/Login.js
@@ -3,22 +3,18 @@ import React, { Component } from 'react'
 export class Login extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="register-logo">
-            Fresh Mart
-          </div>
-        </div>
-        <div className="row login-wrap">
-          <div className="login-box">
-            <h1 style={{ margin: '0px' }}>Login</h1>
-            <form onSubmit={this.handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-              <input type="text" name="emailPhone" placeholder="0853" ref={this.email} />
-              <input type="submit" className="btn" value="Submit" name="Login"  style={{ margin: '5px'}}/>
-            </form>
-            <h3>Atau Dengan</h3>
-            <input type="submit" className="btn" value="Submit" name="Login" />
-
+      <div className="w-full">
+        <div className="flex justify-center my-8 flex-col flex-wrap items-center">
+          <p>Fresh Market</p>
+          <div className="box-border h-96 w-80 p-4 border-2 mt-8 shadow-sm rounded-md">
+            <div className="flex flex-row justify-between mt-10 px-4">
+              <h1 className="text-lg">Masuk</h1> <h1 className="text-lg">Daftar</h1>
+            </div>
+            <div className="flex flex-col mt-6 mx-4 ">
+              <label className="text-sm">Nomor HP atau Email</label>
+              <input type="text" className="w-56 h-10 border-2" value=""/>
+              <input type="submit" className="w-56 mt-2" name="login"/>
+            </div>
           </div>
         </div>
       </div>
